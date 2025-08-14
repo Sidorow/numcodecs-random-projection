dc0e249a6b327127ec3dfefdd2517509da6d475b
Sidorow <sami.sidoroff@helsinki.fi>
2025-08-14 15:24:36 +0300

    more aggressive k estimation for dct

--- src/numcodecs_random_projection/__init__.py
+++ src/numcodecs_random_projection/__init__.py
@@ -111,15 +111,20 @@ class RPCodec(Codec):
         """
 
         D = data.shape[1]
+        data_var = np.var(data)
+
+        normalized_target = target / data_var if data_var > 0 else target
 
         if self.method == "gaussian":
-            ratio = 1 - target
+            ratio = 1 - normalized_target
 
         elif self.method == "dct":
-            ratio = 1 - np.sqrt(target)
+            ratio = 1 - np.sqrt(normalized_target * 8)
 
         estimated_k = int(D * ratio)
-        return max(1, min(estimated_k, D))
+        K = max(1, min(estimated_k, D))
+
+        return K
 
     def _project_blocks(
         self, data: np.ndarray, D: int, K: int, dtype: np.dtype, block_size: int
